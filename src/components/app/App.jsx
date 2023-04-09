eff744801e63be14d96a976ad9b3c5862e563a6a
Volodymyr Proniuk <111579555+vpronyuk@users.noreply.github.com>
2023-04-09 23:43:58 +0300

    deleted unused code

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -27,20 +27,13 @@ export default function App() {
   };
 
   const countTotalFeedbacks = () => {
-    // const { good, neutral, bad } = this.state;
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    // const { good } = this.state;
     const total = countTotalFeedbacks();
     return total > 0 ? Math.round((good / total) * 100) : 0;
   };
-
-  // render() {
-  //   const { good, neutral, bad } = this.state;
-  //   const total = this.countTotalFeedbacks();
-  // const positivePercentage = countPositiveFeedbackPercentage();
 
   return (
     <div>
@@ -67,59 +60,3 @@ export default function App() {
     </div>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   handleFeedbackBtnClick = option => {
-//     this.setState(prevState => ({
-//       [option]: prevState[option] + 1,
-//     }));
-//   };
-
-//   countTotalFeedbacks = () => {
-//     const { good, neutral, bad } = this.state;
-//     return good + neutral + bad;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     const { good } = this.state;
-//     const total = this.countTotalFeedbacks();
-//     return total > 0 ? Math.round((good / total) * 100) : 0;
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-//     const total = this.countTotalFeedbacks();
-//     const positivePercentage = this.countPositiveFeedbackPercentage();
-
-//     return (
-//       <div>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={['good', 'neutral', 'bad']}
-//             onLeaveFeedback={this.handleFeedbackBtnClick}
-//           />
-//         </Section>
-
-//         <Section title="Statistics">
-//           {this.countTotalFeedbacks() > 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={total}
-//               positivePercentage={positivePercentage}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback yet" />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
